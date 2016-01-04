cb07c211287d6e6dc5f55e28586decd734819147
Adrienne Domingus <adrienne.domingus@gmail.com>
2016-01-04 13:16:15 -0700

    wrap in class

--- lib/web_server.rb
+++ lib/web_server.rb
@@ -1,39 +1,62 @@
 require 'socket'
-require 'pry'
-  tcp_server = TCPServer.new(9292)
-  client = tcp_server.accept
 
-  puts "Ready for a request"
-  request_lines = []
-    while line = client.gets and !line.chomp.empty?
-      request_lines << line.chomp
-    end
+class Server
 
-  puts "Got this request:"
-  puts request_lines.inspect
-  # binding.pry
-  greeting = "Hello, World!"
+  attr_accessor :client, :request_lines
 
-  verb = "Verb: #{request_lines[0].split[0]}"
-  path = "Path: #{request_lines[0].split[1]}"
-  protocol = "Protocol: #{request_lines[0].split[2]}"
-  host = "#{request_lines[1]}"
-  port = "Port: #{request_lines[1].split(":")[2]}"
-  origin = "Origin: #{request_lines[1].split(":")[1,2].join(":")}"
-  accept = "#{request_lines[3]}"
-  formatted_request_lines = [verb, path, protocol, host, port, origin, accept]
+  def initialize
+    tcp_server = TCPServer.new(9292)
+    @client = tcp_server.accept
+  end
 
-  puts "sending response."
-  response_1 = "<pre>" + "#{greeting}" + ("\n\n\n") + formatted_request_lines.join("\n") + "</pre>"
-  output = "<html><head></head><body>#{response_1}</body></html>"
-  headers = ["http/1.1 200 ok",
-        "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
-        "server: ruby",
-        "content-type: text/html; charset=iso-8859-1",
-        "content-length: #{output.length}\r\n\r\n"].join("\r\n")
-  # client.puts headers
-  client.puts output
+  def client_request
+    puts "Ready for a request"
+    $request_lines = []
+      while line = @client.gets and !line.chomp.empty?
+        $request_lines << line.chomp
+      end
+  end
 
-  puts ["Wrote this response:", headers, output].join("\n")
-  client.close
-  puts "\nResponse complete, exiting.
+  def greeting
+    puts "Got this request:"
+    puts $request_lines.inspect
+    greeting = "Hello, World!"
+  end
+
+  def to_browser
+    verb = "Verb: #{$request_lines[0].split[0]}"
+    path = "Path: #{$request_lines[0].split[1]}"
+    protocol = "Protocol: #{$request_lines[0].split[2]}"
+    host = "#{$request_lines[1]}"
+    port = "Port: #{$request_lines[1].split(":")[2]}"
+    origin = "Origin: #{$request_lines[1].split(":")[1,2].join(":")}"
+    accept = "#{$request_lines[3]}"
+    @formatted_request_lines = [verb, path, protocol, host, port, origin, accept]
+  end
+
+  def command_line_output
+    to_browser
+    puts "sending response."
+    response_1 = "<pre>" + "#{greeting}" + ("\n\n") + @formatted_request_lines.join("\n") + "</pre>"
+    output = "<html><head></head><body>#{response_1}</body></html>"
+    headers = ["http/1.1 200 ok",
+          "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
+          "server: ruby",
+          "content-type: text/html; charset=iso-8859-1",
+          "content-length: #{output.length}\r\n\r\n"].join("\r\n")
+    client.puts output
+  end
+
+  def close
+    puts ["Wrote this response:", headers, output].join("\n")
+    client.close
+    puts "\nResponse complete, exiting."
+  end
+end
+
+s = Server.new
+s.client_request
+s.greeting
+s.to_browser
+s.command_line_output
+s.close
