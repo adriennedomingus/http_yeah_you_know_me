967135c6ee8d2ad38a31aa5ca5d3e946b7f630a6
Adrienne Domingus <adrienne.domingus@gmail.com>
2016-01-04 17:09:06 -0700

    iteration 2

--- lib/web_server.rb
+++ lib/web_server.rb
@@ -5,18 +5,17 @@ class Server
   attr_accessor :client, :request_lines
 
   def initialize
-    tcp_server = TCPServer.open(9292)
-    @client = tcp_server.accept
+    @tcp_server = TCPServer.open(9292)
     $counter = 0
-    @request_lines = []
   end
 
   def client_request
-    puts "Ready for a request"
     @request_lines = []
+    @client = @tcp_server.accept
     while line = @client.gets and !line.chomp.empty?
       @request_lines << line.chomp
     end
+    puts "Ready for a request"
     $counter += 1
   end
 
@@ -32,15 +31,15 @@ class Server
 
   def command_line_output
     to_browser
-    puts "sending response."
-    response_1 = "<pre>" + @path_output+ "</pre>"
-    @output = "<html><head></head><body>#{response_1}</body></html>"
-    @headers = ["http/1.1 200 ok",
-          "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
-          "server: ruby",
-          "content-type: text/html; charset=iso-8859-1",
-          "content-length: #{@output.length}\r\n\r\n"].join("\r\n")
-    client.puts @output
+     puts "sending response."
+     response_1 = "<pre>" + @path_output+ "</pre>"
+     @output = "<html><head></head><body>#{response_1}</body></html>"
+     @headers = ["http/1.1 200 ok",
+           "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
+           "server: ruby",
+           "content-type: text/html; charset=iso-8859-1",
+           "content-length: #{@output.length}\r\n\r\n"].join("\r\n")
+     client.puts @output
   end
 
   def path
@@ -51,8 +50,7 @@ class Server
       Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')
     when "/shutdown"
       "Total requests: #{$counter}"
-      # close
-    when "/"
+    else
       request_lines = @request_lines
       verb = "Verb: #{request_lines[0].split[0]}"
       path = "Path: #{request_lines[0].split[1]}"
@@ -64,18 +62,20 @@ class Server
       @formatted_request_lines = [verb, path, protocol, host, port, origin, accept]
       greeting + ("\n\n") + @formatted_request_lines.join("\n")
     end
-    # client_request
   end
 
-  # def close
-  #   puts ["Wrote this response:", @headers, @output].join("\n")
-  #   client.close
-  #   puts "\nResponse complete, exiting."
-  # end
+  def close
+    puts ["Wrote this response:", @headers, @output].join("\n")
+    @client.close
+    puts "\nResponse complete, exiting."
+  end
 end
 
-s = Server.new
-s.client_request
-s.greeting
-s.to_browser
-s.command_line_output
+  s = Server.new
+loop do
+  s.client_request
+  s.greeting
+  s.to_browser
+  s.command_line_output
+  s.close
+end
